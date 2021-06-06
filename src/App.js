7ba2a59440e5a6c5b82c5831dcb37ee3325b755a
Andrei Cazaceanu <andrei.cazaceanu@gmail.com>
2021-06-06 20:20:22 +0200

    Adds NewExpense component and Filter component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import {useState} from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
+import ExpensesFilter from './components/ExpensesFilter/ExpensesFilter';
 
 const App = () => {
-  
+  const [year, setyear] = useState(2022);
+ 
+  const expenses = [
+    {
+      id: 1,
+      title: "Car insurance",
+      amount: 295.3,
+      date: new Date(2021, 6, 5),
+    },
+    {
+      id: 2,
+      title: "Charlie",
+      amount: 15.2,
+      date: new Date(2021, 6, 3),
+    },
+    {
+      id: 3,
+      title: "Apartament",
+      amount: 502.3,
+      date: new Date(2021, 6, 1),
+    },
+  ];
+
+  const addExpenseDataHandler = (expenseData) => {
+    console.log(expenseData);
+  }
+
+  const filterExpensesHandler = (date) => {
+    setyear(date);
+  }
+
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses/>
+      <h2>Let's get started!</h2>   
+      <NewExpense onAddExpense={addExpenseDataHandler}/>
+      <ExpensesFilter onFilterExpenses={filterExpensesHandler}/>
+      <Expenses items={expenses} filterExpenses={year}/>
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,44 +2,25 @@ import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 
-const Expenses = () => {
-  const expenses = [
-    {
-      id: 1,
-      title: "Car insurance",
-      amount: 295.3,
-      date: new Date(2021, 6, 5),
-    },
-    {
-      id: 2,
-      title: "Charlie",
-      amount: 15.2,
-      date: new Date(2021, 6, 3),
-    },
-    {
-      id: 3,
-      title: "Apartament",
-      amount: 502.3,
-      date: new Date(2021, 6, 1),
-    },
-  ];
+const Expenses = (props) => {
+  console.log(props.filterExpenses);
 
   return (
     <Card className='expenses'>
       <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
       ></ExpenseItem>
       <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
       ></ExpenseItem>
     </Card>
   );
